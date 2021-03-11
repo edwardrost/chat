8e1b2d9709db2f9dd243ada6455061ce3b39ac7a
Ed Rost <edrost.pro@gmail.com>
2021-03-11 21:36:00 +0300

    add project structure and login & register pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { 
+  BrowserRouter as Router,
+  // Switch, 
+  // Route, 
+  // Link 
+} from "react-router-dom";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './styles/index.scss';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
